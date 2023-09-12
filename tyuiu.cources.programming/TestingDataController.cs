aac49a7e173e35b7375788e64f4e47198a4b0f5d
clipboard1 <simon.serezha1@yandex.ru>
2023-09-12 20:06:48 +0500

    Removed old and added new data
    Also rework GetData method too

--- tyuiu.cources.programming/TestingDataController.cs
+++ tyuiu.cources.programming/TestingDataController.cs
@@ -3,25 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using tyuiu.cources.programming.interfaces;
+//using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace tyuiu.cources.programming
 {
-    public class TestingDataController: ISprint0Task0V0
+    public class TestingDataController
     {
-        private Dictionary<Type, (object result, object[] args)> testData = new() 
+        private Dictionary<Type, (object result, object[] args)> testData = new()
         {
-            { typeof(ISprint0Task0V0), ("54321", new object[] { "12345" })},
-            { typeof(ISprint0Task0V1), (90, new object[] { 10 })},
-            { typeof(ISprint0Task0V2), (20, new object[] { 10, 2 })},
-            { typeof(ISprint1Task0V0), (
-                new int[] { 1, 2, 3 },
-                new object[] { new int[] { 2, 3, 1 }
-            })}        
+
+            { typeof(ISprint1Task0V1), (2.0, new object[] {})},
+            { typeof(ISprint1Task1V0), (2.0, new object[] {1, 4})},
+            { typeof(ISprint1Task2V0), (2, new object[] {2})},
+            { typeof(ISprint1Task3V0), (2.0, new object[] {1, 1})},
+            { typeof(ISprint1Task4V0), (2.0, new object[] {1 , 1 })},
+            { typeof(ISprint1Task6V0), ("", new object[] {"" })},
+            { typeof(ISprint1Task7V0), (2.0, new object[] {1, 1, 1 })},
         };
-        public (object result, object[] args) GetData<T>()
+        public (object result, object[] args) GetData(Type type)
         {
-            return testData[typeof(T)];
+            return testData[type];
         }
 
         public string ReverseString(string str)
