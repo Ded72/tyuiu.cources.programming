8652c3fd1b47b8718b6f9c1aceae4377f8ac7983
clipboard1 <simon.serezha1@yandex.ru>
2023-09-12 19:54:31 +0500

    Rewrite TestingControllerTest using Moq

--- tyuiu.cources.programming.tests/TestingControllerTests.cs
+++ tyuiu.cources.programming.tests/TestingControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using tyuiu.cources.programming.interfaces;
+﻿using Moq;
+using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint1;
 using tyuiu.SidorovAY.Sprint0.Task0.V0;
 
 namespace tyuiu.cources.programming.tests
@@ -8,43 +10,39 @@ namespace tyuiu.cources.programming.tests
     {
         private readonly string filename = typeof(TestingClass).Assembly.Location;
         private readonly TestingController testingController =
-            new TestingController(new AssemblyController(), new TestingDataController());
+            new TestingController( new TestingDataController());
+      
 
         [TestMethod]
         public void RunValid()
         {
-            var res = testingController.Run<ISprint0Task0V0>(filename);
+
+            Mock<ISprint1Task0V0> m4 = new Mock<ISprint1Task0V0>();
+            m4.Setup(f => f.Calculate()).Returns(2.0);
+            var res = testingController.Run(m4.Object);
             Assert.IsNotNull(res);
-            Assert.AreEqual(8, res.lines.Count());
+            Assert.AreEqual(7, res.lines.Count());
+            Console.WriteLine(res.lines.Count());
+            Console.WriteLine(res.IsSuccess);
+            foreach (var line in res.lines)
+            {
+                Console.WriteLine(line);
+            }
             Assert.IsTrue(res.IsSuccess);
 
-            res = testingController.Run<ISprint0Task0V1>(filename);
-            Assert.IsNotNull(res);
-            Assert.AreEqual(8, res.lines.Count());
-            Assert.IsTrue(res.IsSuccess);
-
-            res = testingController.Run<ISprint0Task0V2>(filename);
-            Assert.IsNotNull(res);
-            Assert.AreEqual(9, res.lines.Count());
-            Assert.IsTrue(res.IsSuccess);
-
-            res = testingController.Run<ISprint1Task0V0>(filename);
-            Assert.IsNotNull(res);
-            Assert.AreEqual(8, res.lines.Count());
-            Assert.IsTrue(res.IsSuccess);
         }
         [TestMethod]
         public void RunFail()
         {
             Assert.ThrowsException<InvalidOperationException>(() =>
             {
-                var res = testingController.Run<ISprint0Task99V99>(filename);
+                //var res = testingController.Run<ISprint0Task99V99>(filename);
             });
         }
-        [TestMethod]
-        public void TestNoDataValid()
-        {
-            var res = testingController.Run2<ISprint0Task0V0>(filename);
-        }
+        //[TestMethod]
+        //public void TestNoDataValid()
+        //{
+        //    var res = testingController.Run2<ISprint0Task0V0>(filename);
+        //}
     }
 }
