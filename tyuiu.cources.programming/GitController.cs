0fecc886c93626744d6dbb2ef1e4b7880fe127f3
clipboard1 <simon.serezha1@yandex.ru>
2023-09-12 19:53:12 +0500

    Rewrite repo cloning

--- tyuiu.cources.programming/GitController.cs
+++ tyuiu.cources.programming/GitController.cs
@@ -1,5 +1,6 @@
 ﻿using LibGit2Sharp;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,27 +10,60 @@ using System.Xml.Linq;
 namespace tyuiu.cources.programming
 {
     public class GitController
+
     {
-        public void Load(string repoUrl, string rootDir)
+        public readonly string rootDir;
+
+        public GitController(string rootDir)
         {
-            if ((repoUrl.IndexOf("Tyuiu")) < 0)
-            {
-                throw new FileLoadException($"{repoUrl} - bad link");
-            }
-            var filename = (repoUrl.Substring(repoUrl.IndexOf("Tyuiu"))).Replace(".git", "");
-            var localDir = $@"{rootDir}\{filename}";
-            if (Directory.Exists(localDir))
-            {
-                throw new FileLoadException($"Directory {localDir} already exists!");
-            }
-            
-            Repository.Clone(repoUrl, localDir);
-            //Console.WriteLine($"Copying {repoUrl}");
-            
-
+            this.rootDir = rootDir;
         }
 
+        private void deleteFolder(string folder)
+        {
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(folder);
+                DirectoryInfo[] diA = di.GetDirectories();
+                FileInfo[] fi = di.GetFiles();
+                foreach (FileInfo f in fi)
+                {
+                    f.Attributes = FileAttributes.Normal;
+                    f.Delete();
+                }
+                foreach (DirectoryInfo df in diA)
+                {
+                    deleteFolder(df.FullName);
+                }
+                if (di.GetDirectories().Length == 0 && di.GetFiles().Length == 0) di.Delete();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+        public bool Load(string repoUrl)
+        {
+            try
+            {
+                var filename = Path.GetFileNameWithoutExtension(repoUrl);
+                var localDir = $@"{rootDir}\{filename}";
+
+                if (Directory.Exists(localDir))
+                {
+                    deleteFolder(localDir);
+                }
+                Repository.Clone(repoUrl, localDir);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
 
-        
+
     }
+
+
+}
