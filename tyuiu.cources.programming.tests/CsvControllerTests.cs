b2980d3529d923b1840c803041bec3ef0712024c
clipboard1 <simon.serezha1@yandex.ru>
2023-09-12 19:53:26 +0500

    Create CsvControllerTests.cs

---
+++ tyuiu.cources.programming.tests/CsvControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using LibGit2Sharp;
+using System.Diagnostics;
+using System.IO;
+using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint1;
+using tyuiu.SidorovAY.Sprint0.Task0.V0;
+using Moq;
+
+
+namespace tyuiu.cources.programming.tests
+{
+    [TestClass]
+    public class CsvControllerTests
+    {
+
+        [TestMethod]
+        public void LoadFileValid()
+        {
+            var csvController = new CsvController(new ScoreController(), 
+                new GitController(@"C:\Temp"), 
+                new AssemblyController(),
+                new TestingController(new TestingDataController()));
+            var testingController = new TestingController(new TestingDataController());
+
+            List<object> interfacesFromDll = csvController.Load(@"C:\Temp\links.csv");
+            foreach (var intf in interfacesFromDll)
+            {
+                //Console.WriteLine(intf.GetType().GetInterfaces().First().Name);
+                var result = testingController.Run(intf);
+
+            }
+            ;
+
+        }
+
+        
+
+    }
+}
+
+
