bf329ef8c57b87a2de633a8bd4af9f747b419f59
clipboard1 <simon.serezha1@yandex.ru>
2023-09-14 18:03:07 +0500

    Create app to use

---
+++ TestApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using tyuiu.cources.programming;
+
+
+var csvController = new CsvController(
+                new GitController(@"C:\Temp"),
+                new AssemblyController(),
+                new TestingController(new TestingDataController()));
+
+string path = csvController.Load(@"C:\Temp\0_7_Vyslat_ssylku_s_GitHub-otvety.csv");
+Console.WriteLine($"Путь до отчета - {path}");
