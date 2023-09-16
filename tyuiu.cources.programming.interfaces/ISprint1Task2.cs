b4001ce27c68919275db4def22ad70b88763a9e7
clipboard1 <simon.serezha1@yandex.ru>
2023-09-16 13:00:34 +0500

    Update ISprint1Task2.cs

--- tyuiu.cources.programming.interfaces/ISprint1Task2.cs
+++ tyuiu.cources.programming.interfaces/ISprint1Task2.cs
@@ -12,31 +12,31 @@ namespace tyuiu.cources.programming.interfaces.Sprint1
     public interface ISprint1Task2V3 { int ConvertHourToMin(int value); }
     public interface ISprint1Task2V4 { int CalculateSquare(int value); }
     public interface ISprint1Task2V5 { int CalculateSideSquare(int value); }
-    public interface ISprint1Task2V6 { int ConvertMToKm(double value); }
-    public interface ISprint1Task2V7 { int CalculateSquareCircle(double value); }
+    public interface ISprint1Task2V6 { double ConvertMToKm(int value); }
+    public interface ISprint1Task2V7 { double CalculateSquareCircle(int value); }
     public interface ISprint1Task2V8 { int CalculatePerimetr(int a, int b); }
     public interface ISprint1Task2V9 { double CalculateVolumeCircle(int r); }
-    public interface ISprint1Task2V10 { double Calculate(int value); }
-    public interface ISprint1Task2V11 { int Calculate(int value, int valueTwo); }
-    public interface ISprint1Task2V12 { int Calculate(double value, int valueTwo, double valueThree); }
-    public interface ISprint1Task2V13 { int Calculate(int value); }
-    public interface ISprint1Task2V14 { int Calculate(int value); }
-    public interface ISprint1Task2V15 { int Calculate(int value); }
-    public interface ISprint1Task2V16 { int Calculate(double value); }
-    public interface ISprint1Task2V17 { int Calculate(int value); }
-    public interface ISprint1Task2V18 { int Calculate(double value, int valueTwo); }
-    public interface ISprint1Task2V19 { int Calculate(double value); }
-    public interface ISprint1Task2V20 { int Calculate(int value, int valueTwo); }
-    public interface ISprint1Task2V21 { int Calculate(double value, int valueTwo); }
-    public interface ISprint1Task2V22 { int Calculate(double value, int valueTwo, int valueThree); }
-    public interface ISprint1Task2V23 { int Calculate(double value); }
-    public interface ISprint1Task2V24 { int Calculate(double value, int valueTwo); }
-    public interface ISprint1Task2V25 { double Calculate(double value); }
-    public interface ISprint1Task2V26 { int Calculate(double value, int valueTwo); }
-    public interface ISprint1Task2V27 { int Calculate(int value); }
-    public interface ISprint1Task2V28 { int Calculate(int value); }
-    public interface ISprint1Task2V29 { int Calculate(int value); }
-    public interface ISprint1Task2V30 { double Calculate(double value); }
+    public interface ISprint1Task2V10 { double ConvertMetreToInchs(int value); }
+    public interface ISprint1Task2V11 { int ConvertHoursMinutesToSeconds(int value, int valueTwo); }
+    public interface ISprint1Task2V12 { int CalculateParallelepipedVolume(int value, int valueTwo, int valueThree); }
+    public interface ISprint1Task2V13 { double ConvertMilesToKm(int value); }
+    public interface ISprint1Task2V14 { int ConvertKelvinToCelsius(int value); }
+    public interface ISprint1Task2V15 { int CalculateCubeVolume(int value); }
+    public interface ISprint1Task2V16 { double CalculatePerimetrCircle(int value); }
+    public interface ISprint1Task2V17 { int ConvertMinutesToHours(int value); }
+    public interface ISprint1Task2V18 { int CalculateSideSquareParallelepiped(int value, int valueTwo, int valueThree); }
+    public interface ISprint1Task2V19 { double ConvertInchToKm(int value); }
+    public interface ISprint1Task2V20 { int CalculateSquaresSumm(int value, int valueTwo); }
+    public interface ISprint1Task2V21 { int CalculateRectangleSquare(int value, int valueTwo); }
+    public interface ISprint1Task2V22 { int CalculateAVGValue(int value, int valueTwo, int valueThree); }
+    public interface ISprint1Task2V23 { int ConvertMinutesToSeconds(int value); }
+    public interface ISprint1Task2V24 { int CalculateDiffSquare(int value, int valueTwo); }
+    public interface ISprint1Task2V25 { double ConvertRadsToDegrees(int value); }
+    public interface ISprint1Task2V26 { int CalculateMinutesSinceStart(int value, int valueTwo); }
+    public interface ISprint1Task2V27 { int CalculateSquarePerimetr(int value); }
+    public interface ISprint1Task2V28 { int ConvertCelsiusToKelvin(int value); }
+    public interface ISprint1Task2V29 { int ConvertSecondsToHours(int value); }
+    public interface ISprint1Task2V30 { double ConvertKmToMetre(double value); }
 
 
 }
