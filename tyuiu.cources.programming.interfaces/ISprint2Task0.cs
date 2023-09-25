a533b3ac423bec8a25d7653c6fda08aaf4613410
clipboard1 <simon.serezha1@yandex.ru>
2023-09-25 22:08:39 +0500

    Update sprint2 interfaces
    Changed from cortages to arrays

--- tyuiu.cources.programming.interfaces/ISprint2Task0.cs
+++ tyuiu.cources.programming.interfaces/ISprint2Task0.cs
@@ -6,35 +6,35 @@ using System.Threading.Tasks;
 
 namespace tyuiu.cources.programming.interfaces.Sprint2
 {
-    public interface ISprint2Task0V0 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V1 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V2 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V3 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V4 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V5 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V6 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V7 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V8 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V9 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V10 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V11 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V12 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V13 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V14 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V15 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V16 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V17 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V18 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V19 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V20 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V21 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V22 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V23 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V24 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V25 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V26 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V27 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V28 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V29 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
-    public interface ISprint2Task0V30 { (bool, bool, bool, bool, bool, bool) GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V0 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V1 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V2 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V3 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V4 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V5 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V6 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V7 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V8 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V9 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V10 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V11 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V12 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V13 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V14 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V15 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V16 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V17 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V18 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V19 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V20 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V21 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V22 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V23 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V24 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V25 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V26 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V27 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V28 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V29 {bool[] GetCompareOperations(int x, int y); }
+    public interface ISprint2Task0V30 {bool[] GetCompareOperations(int x, int y); }
 }
--- tyuiu.cources.programming.interfaces/ISprint2Task1.cs
+++ tyuiu.cources.programming.interfaces/ISprint2Task1.cs
@@ -6,35 +6,35 @@ using System.Threading.Tasks;
 
 namespace tyuiu.cources.programming.interfaces.Sprint2
 {
-    public interface ISprint2Task1V0 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V1 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V2 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V3 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V4 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V5 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V6 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V7 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V8 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V9 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V10 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V11 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V12 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V13 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V14 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V15 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V16 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V17 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V18 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V19 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V20 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V21 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V22 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V23 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V24 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V25 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V26 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V27 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V28 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V29 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
-    public interface ISprint2Task1V30 { (bool, bool, bool, bool, bool, bool) GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V0 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V1 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V2 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V3 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V4 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V5 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V6 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V7 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V8 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V9 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V10 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V11 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V12 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V13 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V14 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V15 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V16 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V17 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V18 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V19 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V20 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V21 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V22 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V23 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V24 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V25 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V26 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V27 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V28 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V29 { bool[] GetLogicOperations(int a, int b, int c, int d); }
+    public interface ISprint2Task1V30 { bool[] GetLogicOperations(int a, int b, int c, int d); }
 }
