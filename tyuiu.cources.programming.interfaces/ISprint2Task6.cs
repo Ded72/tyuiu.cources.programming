920c77c34fe459fc41c82d3439bf3fa38b25f019
clipboard1 <simon.serezha1@yandex.ru>
2023-09-27 15:05:05 +0500

    Update ISprint2Task6.cs

--- tyuiu.cources.programming.interfaces/ISprint2Task6.cs
+++ tyuiu.cources.programming.interfaces/ISprint2Task6.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 
 namespace tyuiu.cources.programming.interfaces.Sprint2
 {
-    public interface ISprint2Task6V0 { int FindTariffPayment(int cityCode); }
-    public interface ISprint2Task6V1 { int ImprovisedSign(double x); }
-    public interface ISprint2Task6V2 { double CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task6V3 { double CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task6V4 { double CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task6V5 { double CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task6V6 { double CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task6V7 { double CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task6V8 { double CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task6V9 { double CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task6V10 { double CheckDotInShadedArea(int x, int y); }
+    public interface ISprint2Task6V0 { string FindMonthName(int value); }
+    public interface ISprint2Task6V1 { int FindMonthDaysCount(int value); }
+    public interface ISprint2Task6V2 { string FindMonthSeason(int value); }
+    public interface ISprint2Task6V3 { string FindDayName(int value); }
+    public interface ISprint2Task6V4 { string FindCardSuit(int value); }
+    public interface ISprint2Task6V5 { string FindCardValue(int value); }
+    public interface ISprint2Task6V6 { string FindCardNameAndValue(int value1, int value2); }
+    public interface ISprint2Task6V7 { string FindMonthName(int startYear, int n); }
+    public interface ISprint2Task6V8 { string FindDateOfPreviousDay(int m, int n); }
+    public interface ISprint2Task6V9 { string FindDateOfNextDay(int m, int n); }
+    public interface ISprint2Task6V10 { string FindDateOfPreviousDay(int g, int m, int n); }
+    public interface ISprint2Task6V11 { string FindDateOfNextDay(int g, int m, int n); }
+    public interface ISprint2Task6V12 { double FindDateOfPreviousDay(int g, int m, int n); }
+    public interface ISprint2Task6V13 { double FindDateOfNextDay(int g, int m, int n); }
+    public interface ISprint2Task6V14 { string FindDayName(int value); }
+    public interface ISprint2Task6V15 { string FindDayName(int value); }
 
 }
