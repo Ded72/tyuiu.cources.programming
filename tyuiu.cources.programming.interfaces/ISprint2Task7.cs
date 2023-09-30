3cb643e5655a496886b7bf90efb35a37dfd48f4f
clipboard1 <simon.serezha1@yandex.ru>
2023-09-30 17:02:53 +0500

    Update ISprint2Task7.cs

--- tyuiu.cources.programming.interfaces/ISprint2Task7.cs
+++ tyuiu.cources.programming.interfaces/ISprint2Task7.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 
 namespace tyuiu.cources.programming.interfaces.Sprint2
 {
-    public interface ISprint2Task7V0 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V1 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V2 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V3 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V4 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V5 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V6 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V7 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V8 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V9 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V10 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V11 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V12 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V13 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V14 { bool CheckDotInShadedArea(int x, int y); }
-    public interface ISprint2Task7V15 { bool CheckDotInShadedArea(int x, int y); }
+    public interface ISprint2Task7V0 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V1 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V2 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V3 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V4 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V5 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V6 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V7 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V8 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V9 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V10 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V11 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V12 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V13 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V14 { bool CheckDotInShadedArea(double x, double y); }
+    public interface ISprint2Task7V15 { bool CheckDotInShadedArea(double x, double y); }
 
 
 }
