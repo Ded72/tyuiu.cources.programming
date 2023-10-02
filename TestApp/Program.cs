a21142e9599b2f3f7a6e498f3f478e11929ae2a8
clipboard1 <simon.serezha1@yandex.ru>
2023-10-02 17:26:19 +0500

    Update Program.cs

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -3,9 +3,18 @@
 
 var csvController = new CsvController(
                 "1",
-                new GitController(@"C:\Temp"),
+                new GitController(@"F:\ServiceWorkFolder"),
                 new AssemblyController(),
                 new TestingController(new TestingDataController()));
 
-string path = csvController.Load(@"C:\Temp\test\links.csv");
-Console.WriteLine($"Путь до отчета - {path}");
+var tableReportController = new TableContoller(
+    "1",
+    new GitController(@"F:\ServiceWorkFolder")
+    );
+
+string[] items = csvController.Load(@"D:\Downloads\C# 1 Курс 2023-#1.1 Выслать ссылку с GitHub-ответы.csv");
+
+Console.WriteLine(tableReportController.WriteExcelReport(items));
+
+
+//Console.WriteLine(tableReportController.MergeTables(@"C:\Temp\TableFiles"));
