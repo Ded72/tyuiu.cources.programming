c05c54b653d9912c111f0323608783a3ec08b549
Ded72 <egorivanov789@gmail.com>
2023-10-06 22:31:26 +0500

    add Sprint3

---
+++ tyuiu.cources.programming.interfaces/ISprint3Task0.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.cources.programming.interfaces
+{
+    internal interface ISprint3Task0
+    {
+    }
+}
