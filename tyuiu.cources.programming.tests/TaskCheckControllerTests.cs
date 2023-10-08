3a0906f4665375e40f8cf1980d93a435e05e2eae
clipboard1 <simon.serezha1@yandex.ru>
2023-10-09 01:26:52 +0500

    Update TaskCheckControllerTests.cs
    - added new tests fot TaskCheckController

--- tyuiu.cources.programming.tests/TaskCheckControllerTests.cs
+++ tyuiu.cources.programming.tests/TaskCheckControllerTests.cs
@@ -8,28 +8,125 @@ using Moq;
 
 namespace tyuiu.cources.programming.tests
 {
-    [TestClass]
-    public class CsvControllerTests
-    {
 
+
+    [TestClass]
+    public class TaskCheckControllerTests
+    {
         [TestMethod]
-        public void LoadFileValid()
+        public void InvalidTestData()
         {
             var taskCheckController = new TaskCheckController(
                 "0",
-                new GitController(@"C:\Temp"), 
+                new GitController(@"F:\ServiceWorkFolder"),
                 new AssemblyController(),
                 new TestingController(new TestingDataController()));
-            var testingController = new TestingController(new TestingDataController());
+            string[] items = taskCheckController.LoadLink("");
+            string[] results = File.ReadAllLines(items[0]);
 
-            string[] studentResults = taskCheckController.LoadFile(@"C:\Temp\0_7_Vyslat_ssylku_s_GitHub-otvety.csv");
-            Console.WriteLine(studentResults);
-
-
-
+            Assert.AreEqual("НЕВАЛИДНЫЕ ДАННЫЕ", results[0]);
         }
 
-        
+        [TestMethod]
+        public void InvalidLink()
+        {
+            var taskCheckController = new TaskCheckController(
+               "0",
+               new GitController(@"F:\ServiceWorkFolder"),
+               new AssemblyController(),
+               new TestingController(new TestingDataController()));
+            string[] items = taskCheckController.LoadLink("https://github.com/clipboard1/Tyuiu.SimonSRTests.Sprint");
+
+            string[] results = File.ReadAllLines(items[0]);
+
+            Assert.AreEqual("ССЫЛКА НЕВАЛИДНА", results[1].Split(",")[1]);
+        }
+
+        [TestMethod]
+        public void LibDontExists()
+        {
+            var taskCheckController = new TaskCheckController(
+               "10",
+               new GitController(@"F:\ServiceWorkFolder"),
+               new AssemblyController(),
+               new TestingController(new TestingDataController()));
+            string[] items = taskCheckController.LoadLink("https://github.com/clipboard1/Tyuiu.SimonSRTests.Sprint1");
+            string[] results = File.ReadAllLines(items[0]);
+
+            Assert.AreEqual("НЕТ БИБЛИОТЕКИ У НУЖНОГО ТАСКА", results[1].Split(",")[1]);
+        }
+
+        [TestMethod]
+        public void LibBuildFail()
+        {
+            var taskCheckController = new TaskCheckController(
+                "1",
+                new GitController(@"F:\ServiceWorkFolder"),
+                new AssemblyController(),
+                new TestingController(new TestingDataController()));
+            string[] items = taskCheckController.LoadLink("https://github.com/clipboard1/Tyuiu.SimonSRTests.Sprint1");
+            string[] results = File.ReadAllLines(items[0]);
+
+            Assert.AreEqual("БИБЛИОТЕКА НЕ СКОМПИЛИРОВАЛАСЬ", results[1].Split(",")[1]);
+        }
+
+        [TestMethod]
+        public void InvalidTaskName()
+        {
+            var taskCheckController = new TaskCheckController(
+                "0",
+                new GitController(@"F:\ServiceWorkFolder"),
+                new AssemblyController(),
+                new TestingController(new TestingDataController()));
+            string[] items = taskCheckController.LoadLink("https://github.com/clipboard1/Tyuiu.SimonSRTests.Sprint1");
+            string[] results = File.ReadAllLines(items[0]);
+
+            Assert.AreEqual("НЕКОРРЕКТНОЕ НАЗВАНИЕ ТАСКА", results[1].Split(",")[1]);
+        }
+
+
+        [TestMethod]
+        public void InterfaceLoadFail()
+        {
+            var taskCheckController = new TaskCheckController(
+                "2",
+                new GitController(@"F:\ServiceWorkFolder"),
+                new AssemblyController(),
+                new TestingController(new TestingDataController()));
+            string[] items = taskCheckController.LoadLink("https://github.com/clipboard1/Tyuiu.SimonSRTests.Sprint1");
+            string[] results = File.ReadAllLines(items[0]);
+
+            Assert.AreEqual("ОШИБКА ИНТЕРФЕЙСА", results[1].Split(",")[1]);
+        }
+
+        [TestMethod]
+        public void InvalidAnswers()
+        {
+            var taskCheckController = new TaskCheckController(
+                "3",
+                new GitController(@"F:\ServiceWorkFolder"),
+                new AssemblyController(),
+                new TestingController(new TestingDataController()));
+            string[] items = taskCheckController.LoadLink("https://github.com/clipboard1/Tyuiu.SimonSRTests.Sprint1");
+            string[] results = File.ReadAllLines(items[0]);
+
+            Assert.AreEqual("НЕ СОШЛИСЬ ОТВЕТЫ", results[1].Split(",")[1]);
+        }
+
+        [TestMethod]
+        public void FullValidLaunch()
+        {
+            var taskCheckController = new TaskCheckController(
+                "4",
+                new GitController(@"F:\ServiceWorkFolder"),
+                new AssemblyController(),
+                new TestingController(new TestingDataController()));
+            string[] items = taskCheckController.LoadLink("https://github.com/clipboard1/Tyuiu.SimonSRTests.Sprint1");
+            string[] results = File.ReadAllLines(items[0]);
+
+            Assert.AreEqual("ВСЕ ХОРОШО", results[1].Split(",")[1]);
+        }
+
 
     }
 }
