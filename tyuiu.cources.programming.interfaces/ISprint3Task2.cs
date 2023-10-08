debda31dc9df3b2695591e8560317db69c835ba2
clipboard1 <simon.serezha1@yandex.ru>
2023-10-08 18:35:27 +0500

    Create ISprint3Task2.cs

---
+++ tyuiu.cources.programming.interfaces/ISprint3Task2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.cources.programming.interfaces.Sprint3
+{
+    public interface ISprint3Task2V0 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V1 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V2 { double GetSumSeries(); }
+    public interface ISprint3Task2V3 { double GetSumSeries(); }
+    public interface ISprint3Task2V4 { double GetSumSeries(); }
+    public interface ISprint3Task2V5 { double GetSumSeries(); }
+    public interface ISprint3Task2V6 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V7 { double GetSumSeries(double value); }    
+    public interface ISprint3Task2V8 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V9 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V10 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V11 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V12 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V13 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V14 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V15 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V16 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V17 { double GetSumSeries(); }
+    public interface ISprint3Task2V18 { double GetSumSeries(); }
+    public interface ISprint3Task2V19 { double GetSumSeries(); }
+    public interface ISprint3Task2V20 { double GetSumSeries(); }
+    public interface ISprint3Task2V21 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V22 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V23 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V24 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V25 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V26 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V27 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V28 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V29 { double GetSumSeries(double value); }
+    public interface ISprint3Task2V30 { double GetSumSeries(double value); }
+}
