815f3341cd3a8b3872d5cff5952a18a4cdbc00c5
clipboard1 <simon.serezha1@yandex.ru>
2023-10-09 15:06:00 +0500

    Added new interfaces v0

---
+++ tyuiu.cources.programming.interfaces/ISprint3Task3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.cources.programming.interfaces.Sprint3
+{
+    public interface ISprint3Task3V0 { int GetCharCount(string value, char item); }
+
+}
---
+++ tyuiu.cources.programming.interfaces/ISprint3Task5.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.cources.programming.interfaces.Sprint3
+{
+    public interface ISprint3Task5V0 { double GetSumSumSeries(double x, double startValue1, double startValue2, double stopValue1, double stopValue2); }
+
+}
