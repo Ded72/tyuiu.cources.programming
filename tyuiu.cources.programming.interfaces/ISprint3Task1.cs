a802b24cca0d6b8843eca367210b6f7f7c23d96f
Sergey <128256151+clipboard1@users.noreply.github.com>
2023-10-12 00:16:28 +0500

    Merge pull request #34 from Ded72/master
    Updating interfaces for Task 0 and Task1, adding Task2 and Task3

--- tyuiu.cources.programming.interfaces/ISprint3Task1.cs
+++ tyuiu.cources.programming.interfaces/ISprint3Task1.cs
@@ -7,36 +7,36 @@ using System.Threading.Tasks;
 namespace tyuiu.cources.programming.interfaces.Sprint3
 {
     public interface ISprint3Task1V0 { double GetMultiplySeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task1V1 { double GetMultiplySeries(); }
-    public interface ISprint3Task1V2 { double GetMultiplySeries(); }
-    public interface ISprint3Task1V3 { double GetMultiplySeries(); }
-    public interface ISprint3Task1V4 { double GetMultiplySeries(); }
-    public interface ISprint3Task1V5 { double GetMultiplySeries(); }
-    public interface ISprint3Task1V6 { double GetMultiplySeries(); }
-    public interface ISprint3Task1V7 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V8 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V9 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V10 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V11 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V12 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V13 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V14 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V15 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V16 { double GetMultiplySeries(); }
-    public interface ISprint3Task1V17 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V18 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V19 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V20 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V21 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V22 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V23 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V24 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V25 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V26 { double GetMultiplySeries(); }
-    public interface ISprint3Task1V27 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V28 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V29 { double GetMultiplySeries(double value); }
-    public interface ISprint3Task1V30 { double GetMultiplySeries(); }
+    public interface ISprint3Task1V1 { double GetMultiplySeries(int startValue, int stopValue); }
+    public interface ISprint3Task1V2 { double GetMultiplySeries(int startValue, int stopValue); }
+    public interface ISprint3Task1V3 { double GetMultiplySeries(int startValue, int stopValue); }
+    public interface ISprint3Task1V4 { double GetMultiplySeries(int startValue, int stopValue); }
+    public interface ISprint3Task1V5 { double GetMultiplySeries(int startValue, int stopValue); }
+    public interface ISprint3Task1V6 { double GetMultiplySeries(int startValue, int stopValue); }
+    public interface ISprint3Task1V7 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V8 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V9 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V10 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V11 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V12 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V13 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V14 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V15 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V16 { double GetMultiplySeries(int startValue, int stopValue); }
+    public interface ISprint3Task1V17 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V18 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V19 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V20 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V21 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V22 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V23 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V24 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V25 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V26 { double GetMultiplySeries(int startValue, int stopValue); }
+    public interface ISprint3Task1V27 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V28 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V29 { double GetMultiplySeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task1V30 { double GetMultiplySeries(int startValue, int stopValue); }
 
 
 
