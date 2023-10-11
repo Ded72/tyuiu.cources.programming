820d3ae78f70bc2a578e84ae71f869ac4603efcc
Ded72 <egorivanov789@gmail.com>
2023-10-12 00:03:15 +0500

    addTask3

--- tyuiu.cources.programming.interfaces/ISprint3Task3.cs
+++ tyuiu.cources.programming.interfaces/ISprint3Task3.cs
@@ -7,5 +7,36 @@ using System.Threading.Tasks;
 namespace tyuiu.cources.programming.interfaces.Sprint3
 {
     public interface ISprint3Task3V0 { int GetCharCount(string value, char item); }
+    public interface ISprint3Task3V1 { int GetCharCount(string value, char item); }
+    public interface ISprint3Task3V2 { int GetMaxCharCount(string value, char item); }
+    public interface ISprint3Task3V3 { int GetMinCharCount(string value, char item); }
+    public interface ISprint3Task3V4 { string DeleteCharInString(string value, char item); }
+    public interface ISprint3Task3V5 { string ReplaceCharInString(string value, char replaceable, char replacement); }
+    public interface ISprint3Task3V6 { string ReplaceNumOnChar(string value, char item); }
+    public interface ISprint3Task3V7 { string ReplaceCharOnNum(string value, char replaceable, char replacement); }
+    public interface ISprint3Task3V8 { int ConvertStringToInt(string value); }
+    public interface ISprint3Task3V9 { string ReplaceNumOnChar(string value, char item); }
+    public interface ISprint3Task3V10 { string DeleteCharInString(string value, char item); }
+    public interface ISprint3Task3V11 { string ReplaceCharOnNum(string value, char replaceable, char replacement); }
+    public interface ISprint3Task3V12 { int GetMaxCharCount(string value, char item); }
+    public interface ISprint3Task3V13 { int ConvertStringToInt(string value); }
+    public interface ISprint3Task3V14 { string ReplaceCharInString(string value, char replaceable, char replacement); }
+    public interface ISprint3Task3V15 { int GetMinCharCount(string value, char item); }
+    public interface ISprint3Task3V16 { int GetCharCount(string value, char item); }
+    public interface ISprint3Task3V17 { int ConvertStringToInt(string value); }
+    public interface ISprint3Task3V18 { string ReplaceNumOnChar(string value, char item); }
+    public interface ISprint3Task3V19 { string ReplaceCharOnNum(string value, char replaceable, char replacement); }
+    public interface ISprint3Task3V20 { int GetCharCount(string value, char item); }
+    public interface ISprint3Task3V21 { string ReplaceNumOnChar(string value, char item); }
+    public interface ISprint3Task3V22 { int GetMaxCharCount(string value, char item); }
+    public interface ISprint3Task3V23 { string ReplaceCharOnNum(string value, char replaceable, char replacement); }
+    public interface ISprint3Task3V24 { string ReplaceCharInString(string value, char replaceable, char replacement); }
+    public interface ISprint3Task3V25 { int GetMinCharCount(string value, char item); }
+    public interface ISprint3Task3V26 { int GetCharCount(string value, char item); }
+    public interface ISprint3Task3V27 { int ConvertStringToInt(string value); }
+    public interface ISprint3Task3V28 { string ReplaceNumOnChar(string value, char item); }
+    public interface ISprint3Task3V29 { string DeleteCharInString(string value, char item); }
+    public interface ISprint3Task3V30 { int GetMaxCharCount(string value, char item); }
+
 
 }
