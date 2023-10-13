fa71d1287d8fcdc1947c2239ec690110724286f3
Ded72 <egorivanov789@gmail.com>
2023-10-13 23:26:18 +0500

    addTask6Inter

--- tyuiu.cources.programming.interfaces/ISprint3Task6.cs
+++ tyuiu.cources.programming.interfaces/ISprint3Task6.cs
@@ -4,9 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace tyuiu.cources.programming.interfaces
+namespace tyuiu.cources.programming.interfaces.Sprint3
 {
     internal interface ISprint3Task6
     {
+        public interface ISprint3Task6V0 { double  GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V1 { double  GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V2 { double  GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V3 { double  GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V4 { double  GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V5 { double  GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V6 { double  GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V7 { double  GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V8 { double  GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V9 { double  GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V10 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V11 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V12 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V13 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V14 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V15 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V16 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V17 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V18 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V19 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V20 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V21 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V22 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V23 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V24 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V25 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V26 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V27 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V28 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V29 { double GetDivider(int startValue, int stopValue); }
+        public interface ISprint3Task6V30 { double GetDivider(int startValue, int stopValue); }
+
     }
 }
