a78253be3039729b86ffd0d0b5fcddf086205b6b
clipboard1 <simon.serezha1@yandex.ru>
2023-10-14 12:09:18 +0500

    Added interfaces for 6, 7 tasks

---
+++ tyuiu.cources.programming.interfaces/ISprint3Task7.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.cources.programming.interfaces.Sprint3
+{
+    public interface ISprint3Task7V0 { double[] GetMassFunction(int startValue, int stopValue); }
+}
