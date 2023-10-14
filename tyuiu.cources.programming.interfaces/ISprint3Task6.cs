3316f0d27da3aac43923b5e222dba4ca433542f6
clipboard1 <simon.serezha1@yandex.ru>
2023-10-14 12:09:18 +0500

    Added interfaces for 6, 7 tasks

---
+++ tyuiu.cources.programming.interfaces/ISprint3Task6.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.cources.programming.interfaces.Sprint3
+{
+    public interface ISprint3Task6V0 { int GetSumTheDivisors(int startValue, int stopValue); }
+}
