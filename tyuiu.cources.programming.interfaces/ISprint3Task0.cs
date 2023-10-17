ff28ee798e9078a0384badc6e27758aa842c299f
Ded72 <egorivanov789@gmail.com>
2023-10-17 18:51:01 +0500

    fixTask0Task1

--- tyuiu.cources.programming.interfaces/ISprint3Task0.cs
+++ tyuiu.cources.programming.interfaces/ISprint3Task0.cs
@@ -7,34 +7,34 @@ using System.Threading.Tasks;
 namespace tyuiu.cources.programming.interfaces.Sprint3
 {
     public interface ISprint3Task0V0 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V1 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V2 { double GetSumSeries(int startValue, int stopValue); }
+    public interface ISprint3Task0V1 { double GetSumSeries(int value, int startValue, int stopValue); }
+    public interface ISprint3Task0V2 { double GetMultiplySeries(int startValue, int stopValue); }
     public interface ISprint3Task0V3 { double GetSumSeries(int startValue, int stopValue); }
-    public interface ISprint3Task0V4 { double GetSumSeries(int startValue, int stopValue); }
+    public interface ISprint3Task0V4 { double GetMultiplySeries(int startValue, int stopValue); }
     public interface ISprint3Task0V5 { double GetSumSeries(int startValue, int stopValue); }
-    public interface ISprint3Task0V6 { double GetSumSeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task0V6 { double GetMultiplySeries(int startValue, int stopValue); }
     public interface ISprint3Task0V7 { double GetSumSeries(double value, int startValue, int stopValue); }    
-    public interface ISprint3Task0V8 { double GetSumSeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task0V8 { double GetMultiplySeries(double value, int startValue, int stopValue); }
     public interface ISprint3Task0V9 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V10 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V11 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V12 { double GetSumSeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task0V10 { double GetMultiplySeries(int value, int startValue, int stopValue); }
+    public interface ISprint3Task0V11 { double GetSumSeries(int value, int startValue, int stopValue); }
+    public interface ISprint3Task0V12 { double GetMultiplySeries(int value, int startValue, int stopValue); }
     public interface ISprint3Task0V13 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V14 { double GetSumSeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task0V14 { double GetMultiplySeries(int value, int startValue, int stopValue); }
     public interface ISprint3Task0V15 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V16 { double GetSumSeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task0V16 { double GetMultiplySeries(int startValue, int stopValue); }
     public interface ISprint3Task0V17 { double GetSumSeries(int startValue, int stopValue); }
-    public interface ISprint3Task0V18 { double GetSumSeries(int startValue, int stopValue); }
+    public interface ISprint3Task0V18 { double GetMultiplySeries(int value, int startValue, int stopValue); }
     public interface ISprint3Task0V19 { double GetSumSeries(int startValue, int stopValue); }
-    public interface ISprint3Task0V20 { double GetSumSeries(int startValue, int stopValue); }
+    public interface ISprint3Task0V20 { double GetMultiplySeries(int value, int startValue, int stopValue); }
     public interface ISprint3Task0V21 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V22 { double GetSumSeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task0V22 { double GetMultiplySeries(double value, int startValue, int stopValue); }
     public interface ISprint3Task0V23 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V24 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V25 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V26 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V27 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V28 { double GetSumSeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task0V24 { double GetMultiplySeries(int value, int startValue, int stopValue); }
+    public interface ISprint3Task0V25 { double GetSumSeries(int value, int startValue, int stopValue); }
+    public interface ISprint3Task0V26 { double GetMultiplySeries(int startValue, int stopValue); }
+    public interface ISprint3Task0V27 { double GetSumSeries(int value, int startValue, int stopValue); }
+    public interface ISprint3Task0V28 { double GetMultiplySeries(double value, int startValue, int stopValue); }
     public interface ISprint3Task0V29 { double GetSumSeries(double value, int startValue, int stopValue); }
-    public interface ISprint3Task0V30 { double GetSumSeries(double value, int startValue, int stopValue); }
+    public interface ISprint3Task0V30 { double GetMultiplySeries(int startValue, int stopValue); }
 }
